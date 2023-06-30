67e3119cfd8efbbf70deddef487be674a1acdf3b
issstasevich <pravitel2015ify@gmail.com>
2023-06-30 20:32:15 +0300

    Добавьте файлы проекта.

---
+++ Bodies/CheckForNewBody.cs
@@ -0,0 +1,16 @@
+﻿namespace Server.Bodies
+{
+    public class CheckForNewBody
+    {
+        public int UserId { get; set; } = 0;
+        public int ChatId { get; set; } = 0;
+        // public string AccessToken { get; set; } = string.Empty;
+        public CheckForNewBody() { }
+
+        public CheckForNewBody(int userId, int chatId)
+        {
+            UserId = userId;
+            ChatId = chatId;
+        }
+    }
+}
